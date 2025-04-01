bd5cacac5eb2a279650616aa912f09ecf68b0248
Mr-MysteryMan <mysteriossun18@gmail.com>
2025-04-01 18:40:01 +0800

    T3-V2: add avoid

--- T3/snake_move/src/lib.rs
+++ T3/snake_move/src/lib.rs
@@ -11,6 +11,37 @@ pub struct Pos {
 }
 
 #[wasm_bindgen]
+pub fn add_barriers(n: i32, spos: &[i32], snum: i32, ospos: &[i32], 
+    anum: i32, apos: &[i32]) -> Vec<Pos> {
+    let mut bar: Vec<Pos> = Vec::new();
+    for i in 1..4 {
+        let barrier = Pos{x: spos[(2 * i) as usize], y: spos[(2 * i + 1) as usize], dir: -1, dis: -1};
+        bar.push(barrier);
+    }
+    for i in 0..snum {
+        let barrier = Pos{x: ospos[(8 * i) as usize], y: ospos[(8 * i + 1) as usize], dir: -1, dis: -1};
+        bar.push(barrier);
+        let barrier = Pos{x: ospos[(8 * i + 2) as usize], y: ospos[(8 * i + 3) as usize], dir: -1, dis: -1};
+        bar.push(barrier);
+        let barrier = Pos{x: ospos[(8 * i + 4) as usize], y: ospos[(8 * i + 5) as usize], dir: -1, dis: -1};
+        bar.push(barrier);
+        let barrier = Pos{x: ospos[(8 * i + 6) as usize], y: ospos[(8 * i + 7) as usize], dir: -1, dis: -1};
+        bar.push(barrier);
+    }
+    for i in 0..n {
+        let barrier = Pos{x: 0, y: i + 1, dir: -1, dis: -1};
+        bar.push(barrier);
+        let barrier = Pos{x: i + 1, y: 0, dir: -1, dis: -1};
+        bar.push(barrier);
+        let barrier = Pos{x: n + 1, y: i + 1, dir: -1, dis: -1};
+        bar.push(barrier);
+        let barrier = Pos{x: i + 1, y: n + 1, dir: -1, dis: -1};
+        bar.push(barrier);
+    }
+    return bar
+}
+
+#[wasm_bindgen]
 pub fn snake_BFS(src: Pos, dst: Pos, bar: Vec<Pos>, 
     mut queue: Vec<Pos>, mut visited: Vec<Pos>) -> Pos {
     if dst.x == src.x && dst.y == src.y {
@@ -56,7 +87,7 @@ pub fn snake_BFS(src: Pos, dst: Pos, bar: Vec<Pos>,
 }
 
 #[wasm_bindgen]
-pub fn cango(src: Pos, bar: Vec<Pos>) -> Vec<i32> {
+pub fn dir_nobar(src: Pos, bar: Vec<Pos>) -> Vec<i32> {
     let mut result: Vec<i32> = Vec::new();
     if !bar.iter().any(|p| p.x == src.x && p.y == src.y + 1) {
         result.push(0);
@@ -74,35 +105,66 @@ pub fn cango(src: Pos, bar: Vec<Pos>) -> Vec<i32> {
 }
 
 #[wasm_bindgen]
+pub fn dir_nosnake(src: Pos, snum: i32, snakes: Vec<Pos>) -> Vec<i32> {
+    let mut result: Vec<i32> = Vec::new();
+    for i in 0..snum {
+        let snake = *(&snakes[i as usize]);
+        if !(snake.x == src.x + 1 && snake.y == src.y + 1) 
+        && !(snake.x == src.x - 1 && snake.y == src.y + 1) 
+        && !(snake.x == src.x && snake.y == src.y + 2) {
+            result.push(0);
+        }
+        if !(snake.x == src.x - 1 && snake.y == src.y - 1) 
+        && !(snake.x == src.x - 1 && snake.y == src.y + 1) 
+        && !(snake.x == src.x - 2 && snake.y == src.y) {
+            result.push(1);
+        }
+        if !(snake.x == src.x - 1 && snake.y == src.y - 1) 
+        && !(snake.x == src.x + 1 && snake.y == src.y - 1) 
+        && !(snake.x == src.x && snake.y == src.y - 2) {
+            result.push(2);
+        }
+        if !(snake.x == src.x + 1 && snake.y == src.y + 1) 
+        && !(snake.x == src.x + 1 && snake.y == src.y - 1) 
+        && !(snake.x == src.x + 2 && snake.y == src.y) {
+            result.push(3);
+        }
+    }
+    return result
+}
+
+#[wasm_bindgen]
+pub fn cango(src: Pos, snum: i32, bar: Vec<Pos>, snakes: Vec<Pos>) -> Vec<i32> {
+    let mut result: Vec<i32> = Vec::new();
+    let nobar = dir_nobar(src, bar);
+    let nosnake = dir_nosnake(src, snum, snakes);
+    for i in 0..4 {
+        if nobar.iter().any(|&p| p == i) 
+        && nosnake.iter().any(|&p| p == i) {
+            result.push(i);
+        } 
+    }
+    return result
+}
+
+#[wasm_bindgen]
+pub fn react_to_nodir(dir: i32, can_dir: Vec<i32>) -> i32 {
+    if dir == -1 {
+        for i in 0..4 {
+            if can_dir.iter().any(|&p| p == i) {
+                return i
+            }
+        }
+    }
+    return dir
+}
+
+#[wasm_bindgen]
 pub fn greedy_snake_step(n: i32, spos: &[i32], snum: i32, ospos: &[i32], 
     anum: i32, apos: &[i32], round: i32) -> i32 {
-    let mut bar: Vec<Pos> = Vec::new();
+    let mut bar = add_barriers(n, spos, snum, ospos, anum, apos);
     let mut queue: Vec<Pos> = Vec::new();
     let mut visited: Vec<Pos> = Vec::new();
-    for i in 1..4 {
-        let barrier = Pos{x: spos[(2 * i) as usize], y: spos[(2 * i + 1) as usize], dir: -1, dis: -1};
-        bar.push(barrier);
-    }
-    for i in 0..snum {
-        let barrier = Pos{x: ospos[(8 * i) as usize], y: ospos[(8 * i + 1) as usize], dir: -1, dis: -1};
-        bar.push(barrier);
-        let barrier = Pos{x: ospos[(8 * i + 2) as usize], y: ospos[(8 * i + 3) as usize], dir: -1, dis: -1};
-        bar.push(barrier);
-        let barrier = Pos{x: ospos[(8 * i + 4) as usize], y: ospos[(8 * i + 5) as usize], dir: -1, dis: -1};
-        bar.push(barrier);
-        let barrier = Pos{x: ospos[(8 * i + 6) as usize], y: ospos[(8 * i + 7) as usize], dir: -1, dis: -1};
-        bar.push(barrier);
-    }
-    for i in 0..n {
-        let barrier = Pos{x: 0, y: i + 1, dir: -1, dis: -1};
-        bar.push(barrier);
-        let barrier = Pos{x: i + 1, y: 0, dir: -1, dis: -1};
-        bar.push(barrier);
-        let barrier = Pos{x: n + 1, y: i + 1, dir: -1, dis: -1};
-        bar.push(barrier);
-        let barrier = Pos{x: i + 1, y: n + 1, dir: -1, dis: -1};
-        bar.push(barrier);
-    }
     let mut scores: Vec<i32> = Vec::new();
     let mut actions: Vec<i32> = Vec::new();
     for i in 0..anum {
@@ -128,7 +190,12 @@ pub fn greedy_snake_step(n: i32, spos: &[i32], snum: i32, ospos: &[i32],
     let mut result = -1;
     let mut max_score = -999;
     let my_pos = Pos{x: spos[0], y: spos[1], dir: -1, dis: -1};
-    let can_dir = cango(my_pos, bar.clone());
+    let mut snakes: Vec<Pos> = Vec::new();
+    for i in 0..snum {
+        let snake = Pos{x: ospos[(8 * i) as usize], y: ospos[(8 * i + 1) as usize], dir: -1, dis: -1};
+        snakes.push(snake);
+    }
+    let can_dir = cango(my_pos, snum, bar.clone(), snakes);
     for i in 0..anum {
         let score_i = scores[i as usize];
         let action_i = actions[i as usize];
@@ -137,9 +204,8 @@ pub fn greedy_snake_step(n: i32, spos: &[i32], snum: i32, ospos: &[i32],
             result = action_i;
         }
     }
-    if result == -1 {
-        result = 0
-    }
+    let nobar = dir_nobar(my_pos, bar.clone());
+    result = react_to_nodir(result, nobar);
     return result
 }
 
